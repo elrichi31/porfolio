9e2ff543b161035dc8e76f0438e05bcc2d1127ff
Nicolas <Nicolas>
2023-11-28 23:13:51 -0500

    Add social media

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 function Footer() {
     return (
@@ -11,8 +12,17 @@ function Footer() {
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">AlphaBit</span>
                     </a>
                     <ul className="flex flex-col [@media(min-width:605px)]:flex-row items-center text-[@media(min-width:605px)] font-medium text-gray-500 [@media(min-width:605px)]:mt-0 dark:text-gray-400 [@media(min-width:605px)]:space-y-0">
-                        <li>
-                            <a href="/contact" className="mb-2 [@media(min-width:605px)]:mb-0 mr-0 [@media(min-width:605px)]:mr-3 hover:underline md:mr-6">Contact</a>
+                        <li className='flex items-center'>
+                            <a href="https://www.twitter.com/nicolas_moina" className="mb-2 [@media(min-width:605px)]:mb-0 mr-0 [@media(min-width:605px)]:mr-3 hover:underline md:mr-6"><FaTwitter className="text-3xl"></FaTwitter></a>
+                        </li>
+                        <li className='flex items-center'>
+                            <a href="https://www.instagram.com/nicolas.moina" className="mb-2 [@media(min-width:605px)]:mb-0 mr-0 [@media(min-width:605px)]:mr-3 hover:underline md:mr-6"><FaInstagram className="text-3xl"></FaInstagram></a>
+                        </li>
+                        <li className='flex items-center'>
+                            <a href="https://www.github.com/elrichi31" className="mb-2 [@media(min-width:605px)]:mb-0 mr-0 [@media(min-width:605px)]:mr-3 hover:underline md:mr-6"><FaGithub className="text-3xl"></FaGithub></a>
+                        </li>
+                        <li className='flex items-center'>
+                            <a href="https://www.linkedin.com/in/nicolas-moina-21a85b20a" className="mb-2 [@media(min-width:605px)]:mb-0 mr-0 [@media(min-width:605px)]:mr-3 hover:underline md:mr-6"><FaLinkedin className="text-3xl"></FaLinkedin></a>
                         </li>
                     </ul>
                 </div>
